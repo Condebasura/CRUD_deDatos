a5718bd5f3223cc7a7cbf868c08fe454cc52611e
Pablo <could_2032@outlook.com>
2023-09-09 02:52:57 -0300

    primer commit

---
+++ js/DatosFemenino.js
@@ -0,0 +1,97 @@
+const RecibeDatos = document.querySelector("[data-recibe]"),
+
+$fragment = document.createDocumentFragment();
+
+
+ // El tr que va dentro de $thead, th que es el encabezado para cada columna de datos 
+// datos en version larga...
+const $table = document.createElement("table");
+const $thead = document.createElement("thead");
+const $tr_head = document.createElement("tr");
+
+$table.appendChild($thead);
+$thead.appendChild($tr_head);
+
+// se creara el tbody con su tr.
+
+axios.get("http://localhost:3004/Mujer")
+  .then(res => {
+    let json = res.data;
+
+    // Metodo resumido utilizando for
+    const NombColumn = ["Nombre", "Apelido","Email", "Sexo", "Nacimiento", "Telefono", "Direccion", "Ciudad", "Provincia", "Pais", "Editar", "Eliminar"];
+    for (let i = 0; i < NombColumn.length; i++) {
+      let total = NombColumn[i];
+      let th_head = document.createElement("th");
+      $tr_head.appendChild(th_head);
+      th_head.textContent = total;
+    }
+    const tbody = document.createElement("tbody");
+    $table.appendChild(tbody);
+
+    json.forEach(el => {
+      //encontrar la forma de que se creen los td en el trinea depndiendo de la cantidad de datos en e json
+
+      let tr = tbody.insertRow();
+      let tdNonmb = tr.insertCell();
+      let tdApll = tr.insertCell();
+      let tdemail =tr.insertCell();
+      let tdSexo = tr.insertCell();
+      let tdEdad = tr.insertCell();
+      let tdTel = tr.insertCell();
+      let tdDirec = tr.insertCell();
+      let tdCity = tr.insertCell();
+      let tdProv = tr.insertCell();
+      let tdPais = tr.insertCell();
+
+      tdNonmb.textContent = el.nombre;
+      tdApll.textContent = el.apellido;
+      tdemail.textContent = el.email;
+      tdSexo.textContent = el.sexo;
+      tdEdad.textContent = el.edad;
+       tdTel.textContent = el.tel;
+       tdDirec.textContent = el.direc;
+       tdCity.textContent = el.city;
+       tdProv.textContent = el.prov;
+       tdPais.textContent = el.pais;
+      tr.setAttribute("class", "linea");
+      tbody.appendChild(tr);
+    
+      
+     let Edit = tr.insertCell();
+       let Delet = tr.insertCell();
+       let $btnEdit = document.createElement("a");
+       let $btnDelet = document.createElement("a");
+       Edit.appendChild($btnEdit);
+       Delet.appendChild($btnDelet);
+      $btnDelet.textContent = "Eliminar";
+      $btnEdit.textContent = "Editar";
+      $btnDelet.setAttribute("class", "del");
+      $btnEdit.setAttribute("class", "edit");
+      // creando el ipervinculo al editor con set!!    
+       $btnEdit.setAttribute("href", `../html/Editar.html?id=${el.id}&desdepagina=../html/Muj.html`);
+    
+                 // programamos el ipervinculo de eliminacion
+                 $btnDelet.addEventListener("click", (e)=>{
+                  e.preventDefault();
+                  if(e.target.matches(".del")){
+                    let isDelete = confirm(`¿Esta seguro de eliminar los datos de ${el.nombre} ${el.apellido}?`)
+                  if(isDelete){
+                    axios.delete(`http://localhost:3004/Mujer/${el.id}`)
+                    .then(res => location.reload())
+                    .catch(err => console.error(err))
+                  }
+                  
+                  }
+                })
+    
+      
+       $fragment.appendChild($table);
+
+
+    })
+    RecibeDatos.appendChild($fragment);
+
+
+  })
+  .catch(err => console.error(err));
---
+++ js/Envio.js
@@ -0,0 +1,258 @@
+// creacion de variables para los diferentes imputs del formulario
+const formulario = document.querySelector(".form");
+const nombre = document.querySelector(".nombre");
+const apellido = document.querySelector(".ap");
+const email = document.querySelector(".mail");
+let sexo = document.querySelector("[data-sexo]");
+let edad = document.querySelector(".naci");
+const tel = document.querySelector(".tel");
+const direc = document.querySelector(".direc");
+const city = document.querySelector(".city");
+const prov = document.querySelector(".prov");
+const pais = document.querySelector(".pais");
+let Hombre = document.querySelector("[data-hombre]");
+let Mujer = document.querySelector("[data-mujer]");
+const btn_submit = document.querySelector(".btnEnv");
+// Se crean  RegEx
+
+//  Para emails.
+const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Para solo letras.
+const regexLetras = /^[a-z A-Z\\s]+$/;
+
+// Para letras y hasta 5 digitos, para direccion.
+const regexLetrasNum = /^\w+\s+\w{0,5}$/;
+
+// validaciones para el formulario
+const validaNombre = () => {
+    if (!regexLetras.test(nombre.value)) {
+        nombre.style.border = " 2px solid tomato";
+        nombre.setCustomValidity("El campo no puede estar vacio contener numeros o caracteres especiales");
+        return false;
+    } else {
+        nombre.style.border = "none";
+        nombre.setCustomValidity("");
+        return true;
+    }
+};
+
+nombre.addEventListener("input", validaNombre);
+
+const validaApellido = () => {
+    if (!regexLetras.test(apellido.value)) {
+        apellido.style.border = " 2px solid tomato";
+        apellido.setCustomValidity(`El campo no puede estar vacio contener numeros o caracteres especiales`);
+        return false;
+    } else {
+        apellido.style.border = "none";
+        apellido.setCustomValidity("");
+        return true;
+    }
+};
+
+apellido.addEventListener("input", validaApellido);
+
+const validaEmail = () => {
+    if (!regexEmail.test(email.value)) {
+        email.style.border = "2px solid tomato";
+        email.setCustomValidity("El campo no puede estar vacio contener numeros o caracteres especiales");
+        return false;
+    } else {
+        email.style.border = "none";
+        email.setCustomValidity("");
+        return true;
+    }
+};
+
+email.addEventListener("input", validaEmail);
+
+const Elsexo = () => {
+    if(sexo.value == ""){
+        return false;
+ }
+    if (sexo.value == "Masculino"){
+        sexo = sexo.value;
+    return true;
+}
+if (sexo.value == "Femenino"){
+    sexo = sexo.value;
+    return true;
+}
+
+};
+
+const ValidarFecha =() =>{
+    const max = new Date().getFullYear('dd/mm/yyyy') + '-12-31';
+    edad.setAttribute("max", max);
+   if(edad > max){
+        return false;
+    }else{
+        
+        return true;
+    }
+
+    
+};
+
+edad.addEventListener("input", ValidarFecha);
+
+const Valtel = () =>{
+    if(tel.value == "" || tel.value.length > 12){
+        tel.style.border = "2px solid tomato";
+        tel.setCustomValidity("Ingreso mas de 12 digitos o un numero invalido");
+        return false;
+    }else{
+        tel.style.border = "none";
+        tel.setCustomValidity("");
+        return true;
+    }
+};
+tel.addEventListener("input", Valtel);
+
+const Validardireccion = () =>{
+    if (!regexLetrasNum.test(direc.value)) {
+        direc.style.border = " 2px solid tomato";
+        direc.setCustomValidity(`El campo no puede estar vacio contener  caracteres especiales o mas de 5 números`);
+        return false;
+    } else {
+        direc.style.border = "none";
+        direc.setCustomValidity("");
+        return true;
+    }
+};
+direc.addEventListener("input", Validardireccion);
+
+const ValidaCiudad = () =>{
+    if (!regexLetras.test(city.value)) {
+        city.style.border = " 2px solid tomato";
+        city.setCustomValidity(`El campo no puede estar vacio contener numeros o caracteres especiales`);
+        return false;
+    } else {
+        city.style.border = "none";
+        city.setCustomValidity("");
+        return true;
+    }
+};
+
+city.addEventListener("input", ValidaCiudad);
+
+const validaProvincia =()=>{
+    if (!regexLetras.test(prov.value)) {
+        prov.style.border = " 2px solid tomato";
+        prov.setCustomValidity(`El campo no puede estar vacio contener numeros o caracteres especiales`);
+        return false;
+    } else {
+        prov.style.border = "none";
+        prov.setCustomValidity("");
+        return true;
+    }
+};
+prov.addEventListener("input", validaProvincia);
+
+const ValidaPais = ()=>{
+    if (!regexLetras.test(pais.value)) {
+        pais.style.border = " 2px solid tomato";
+        pais.setCustomValidity(`El campo no puede estar vacio contener numeros o caracteres especiales`);
+        return false;
+    } else {
+        pais.style.border = "none";
+        pais.setCustomValidity("");
+        return true;
+    }
+};
+
+pais.addEventListener("input",ValidaPais);
+
+// Creamos la programacion de el evento submit
+formulario.addEventListener("submit", (e) => {
+    // cancelamos el comportamiento por defecto
+    e.preventDefault();
+
+    //si las validaciones no se cumplen se cancela el envio del formulario
+    if (!validaNombre() || !validaApellido() || !validaEmail() || Elsexo() || ValidarFecha() || Valtel()||Validardireccion() || ValidaCiudad() || validaProvincia()|| ValidaPais()) {
+    e.preventDefault();
+    }
+
+    // Funcion para cambiar l formato en que el archivo json recibe la fecha, de yyyy/mm/dd a dd/mm/yyyy
+    function changeDateFormat(date) {
+        const dateParts = date.split('-');
+        return `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
+      };
+
+      //Funcion para agregar los datos al json
+     function addDatos(nombre, apellido, email, sexo, edad, tel, direc, city, prov, pais) {
+        // ejecuta la funcion que cambia el formato de fecha en edad
+       edad = changeDateFormat(edad);
+
+       // dependiendo de que sexo elijas, Masculino / Femenino, se envia en el archivo json
+         if (sexo == "Masculino") {
+             return fetch(`http://localhost:3004/Hombre`, {
+                 method: "POST",
+                 headers: {
+                     "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ nombre, apellido, email, sexo, edad, tel, direc, city, prov, pais, id: uuid.v4() })
+            })
+            .then(res => {
+
+                let json = res.data;
+               
+                           // si el status es ok al ingresar los datos aparece un aviso de que los datos an ingresados correctamente.
+                    if (res.status >= 201 && res.status <= 300) {
+                        const Exito = document.createElement("p");
+                        Exito.textContent = "Datos Ingresados!!";
+                        Exito.setAttribute("class", "exito");
+
+                        setTimeout(() =>{ formulario.appendChild(Exito), location.reload(), 100000});
+                    }
+                    
+                })
+
+                .catch(err => console.log(err));
+
+        };
+
+
+
+        if (sexo == "Femenino") {
+            return fetch(`http://localhost:3004/Mujer`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ nombre, apellido, email, sexo, edad, tel, direc, city, prov, pais, id: uuid.v4() })
+            })
+            .then(res => {
+                       
+                if (res.status >= 201 && res.status <= 300) {
+                    const Exito = document.createElement("p");
+                    Exito.textContent = "Datos Ingresados!!";
+                    Exito.setAttribute("class", "exito");
+
+                    setTimeout(formulario.appendChild(Exito), location.reload(), 10000);
+                }
+            })
+
+            .catch(err => console.log(err));
+        }
+        
+        
+    }
+
+    
+   
+    
+    
+
+//RegEx para email (^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$)
+
+
+
+addDatos(nombre.value, apellido.value, email.value, sexo,edad.value , tel.value, direc.value, city.value, prov.value, pais.value);
+
+
+});
+
+
+
