1d78aa511c3e486a2ada2dab4fe7d1682d1102bb
Pablo Zabala <could_2032@outlook.com>
2024-01-28 06:05:26 -0300

    Cambiando archivos de html a pug, creando el backend y las rutas

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import {fileURLToPath} from "url";
+import taskController from "./controllers/taskController.js";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get("/", taskController.getForm);
+
+app.listen(port , ()=>{
+	console.log(`La aplicacion esta funcionando en http://localhost:${port}`);
+});
