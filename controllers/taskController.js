e08bc33bf90e02072803eea86bac7cdc6b9d7054
Pablo Zabala <could_2032@outlook.com>
2024-01-28 21:45:44 -0300

    Creando el formulario para el ingreso de datos

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,7 +1,7 @@
 
 
 const getForm = (req,res) =>{
-    res.render("index", {title: "CRUD de Datos" , logo:"form", EstasEn: "Ingreso de Datos" , SexoM:"Masculino", SexoF: "Femenino"})
+    res.render("index", {title: "CRUD de Datos" , logo:"form", EstasEn: "Ingreso de Datos" , SexoM:"Masculino", SexoF: "Femenino", labelN:"Nombre"})
 };
 
 export default{
