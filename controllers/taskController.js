dba766d40ddefdcdb6837a8f178debd2e56a622d
Pablo Zabala <could_2032@outlook.com>
2024-01-29 00:11:24 -0300

    Continuando la creacion del formulario

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,7 +1,7 @@
 
 
 const getForm = (req,res) =>{
-    res.render("index", {title: "CRUD de Datos" , logo:"form", EstasEn: "Ingreso de Datos" , SexoM:"Masculino", SexoF: "Femenino", labelN:"Nombre"})
+    res.render("index", {title: "CRUD de Datos" , logo:"form", EstasEn: "Ingreso de Datos" , SexoM:"Masculino", SexoF: "Femenino", LNombre:"Nombre", LApellido:"Apellido", LEmail:"Email", LSexo:"Sexo", LNacimiento:"Fecha de Nacimiento", LTel:"Telefono", LDirec:"Direccion", LCiudad:"Ciudad", LProvincia:"Provincia", LPais:"Pais", boton:"Enviar"})
 };
 
 export default{
