e27acdcac6bed3020cd0900b315ae179b22c0d1e
Pablo Zabala <could_2032@outlook.com>
2024-02-02 09:04:07 -0300

    dejando master como estaba

--- controllers/taskController.js
+++
@@ -1,9 +0,0 @@
-
-
-const getForm = (req,res) =>{
-    res.render("index", {title: "CRUD de Datos" , logo:"form", EstasEn: "Ingreso de Datos" , SexoM:"Masculino", SexoF: "Femenino", labelN:"Nombre"})
-};
-
-export default{
-	getForm
-};
